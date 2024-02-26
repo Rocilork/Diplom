0e43e264f74a2177efcb4bb3012250612510115c
Igor <Igor@mail.ru>
2024-02-26 23:05:57 +0600

    Commit1

---
+++ app/src/main/java/com/example/electronicmagazine/Administrator.kt
@@ -0,0 +1,11 @@
+package com.example.electronicmagazine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Administrator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_administrator)
+    }
+}
